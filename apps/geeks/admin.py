fa5a282b20b78cf3d7de1432490840036501504b
Aknazar120 <aknazarumaraliev07@gmail.com>
2024-10-09 20:21:27 +0600

    первый коммит

---
+++ apps/geeks/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Settings
+
+# Register your models here.
+@admin.register(Settings)
+class SettingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description']
+    
